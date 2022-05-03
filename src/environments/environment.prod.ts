9e01193eb02073993e68904c400e6aee61669e76
Danny <live626@gmail.com>
2022-05-03 17:43:48 -0400

    environment parameter added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  HOST: 'http://${HOST}/consigneeRegistration',
+  PATH_ROOT: '/consigneeRegistration',
+  REINTENTOS: 3,
+  TOKEN_AUTH_USERNAME: 'consigneeRegistration',
+  TOKEN_AUTH_PASSWORD: '${TOKEN_AUTH_PASSWORD}',
+  TOKEN_NAME: 'access_token'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,11 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  HOST: 'http://${HOST}/consigneeRegistration',
+  PATH_ROOT: '/consigneeRegistration',
+  REINTENTOS: 3,
+  TOKEN_AUTH_USERNAME: 'consigneeRegistration',
+  TOKEN_AUTH_PASSWORD: '${TOKEN_AUTH_PASSWORD}',
+  TOKEN_NAME: 'access_token'
 };
